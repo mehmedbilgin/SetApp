ff41aac5534471bc9d54b36726bdf35c22fbf5c4
Kadir Kartal <kadirkartal13@hotmail.com>
2022-01-18 23:15:44 +0300

    - bitlis

--- SetApp/StokEkle.Designer.cs
+++ SetApp/StokEkle.Designer.cs
@@ -35,7 +35,7 @@ namespace SetApp
             this.sekil = new System.Windows.Forms.ComboBox();
             this.dokum = new System.Windows.Forms.ComboBox();
             this.adet = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -49,163 +49,143 @@ namespace SetApp
             // tur
             // 
             this.tur.FormattingEnabled = true;
-            this.tur.Items.AddRange(new object[] {
-            "PN",
-            "DN"});
-            this.tur.Location = new System.Drawing.Point(169, 86);
+            this.tur.Location = new System.Drawing.Point(148, 64);
+            this.tur.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tur.Name = "tur";
-            this.tur.Size = new System.Drawing.Size(151, 28);
+            this.tur.Size = new System.Drawing.Size(133, 23);
             this.tur.TabIndex = 0;
             // 
             // olcu
             // 
             this.olcu.FormattingEnabled = true;
-            this.olcu.Items.AddRange(new object[] {
-            "10",
-            "16",
-            "20",
-            "30",
-            "40",
-            "50",
-            "60",
-            "80",
-            "100",
-            "120",
-            "150",
-            "200",
-            "250",
-            "300"});
-            this.olcu.Location = new System.Drawing.Point(169, 146);
+            this.olcu.Location = new System.Drawing.Point(148, 110);
+            this.olcu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.olcu.Name = "olcu";
-            this.olcu.Size = new System.Drawing.Size(151, 28);
+            this.olcu.Size = new System.Drawing.Size(133, 23);
             this.olcu.TabIndex = 1;
             // 
             // sertifika
             // 
             this.sertifika.FormattingEnabled = true;
-            this.sertifika.Items.AddRange(new object[] {
-            "CART SERTİFİKASI",
-            "CURT SERTİFİKASI",
-            "-NON CERTİFİCATE-"});
-            this.sertifika.Location = new System.Drawing.Point(169, 212);
+            this.sertifika.Location = new System.Drawing.Point(148, 159);
+            this.sertifika.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.sertifika.Name = "sertifika";
-            this.sertifika.Size = new System.Drawing.Size(151, 28);
+            this.sertifika.Size = new System.Drawing.Size(133, 23);
             this.sertifika.TabIndex = 2;
             // 
             // sekil
             // 
             this.sekil.FormattingEnabled = true;
-            this.sekil.Items.AddRange(new object[] {
-            "DÜZ",
-            "KÖŞE",
-            "FIRTINA"});
-            this.sekil.Location = new System.Drawing.Point(169, 279);
+            this.sekil.Location = new System.Drawing.Point(148, 209);
+            this.sekil.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.sekil.Name = "sekil";
-            this.sekil.Size = new System.Drawing.Size(151, 28);
+            this.sekil.Size = new System.Drawing.Size(133, 23);
             this.sekil.TabIndex = 3;
             // 
             // dokum
             // 
             this.dokum.FormattingEnabled = true;
-            this.dokum.Items.AddRange(new object[] {
-            "PİK",
-            "SFERO"});
-            this.dokum.Location = new System.Drawing.Point(561, 86);
+            this.dokum.Location = new System.Drawing.Point(491, 64);
+            this.dokum.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dokum.Name = "dokum";
-            this.dokum.Size = new System.Drawing.Size(151, 28);
+            this.dokum.Size = new System.Drawing.Size(133, 23);
             this.dokum.TabIndex = 4;
             // 
             // adet
             // 
-            this.adet.Location = new System.Drawing.Point(561, 147);
+            this.adet.Location = new System.Drawing.Point(491, 110);
+            this.adet.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.adet.Name = "adet";
-            this.adet.Size = new System.Drawing.Size(151, 27);
+            this.adet.Size = new System.Drawing.Size(133, 23);
             this.adet.TabIndex = 5;
             // 
-            // button1
+            // btn_add
             // 
-            this.button1.Location = new System.Drawing.Point(584, 272);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 35);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "EKLE";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_add.Location = new System.Drawing.Point(511, 204);
+            this.btn_add.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(88, 26);
+            this.btn_add.TabIndex = 6;
+            this.btn_add.Text = "EKLE";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(76, 94);
+            this.label1.Location = new System.Drawing.Point(66, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.Size = new System.Drawing.Size(34, 15);
             this.label1.TabIndex = 7;
             this.label1.Text = "TÜR :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(477, 154);
+            this.label2.Location = new System.Drawing.Point(417, 116);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.Size = new System.Drawing.Size(41, 15);
             this.label2.TabIndex = 8;
             this.label2.Text = "ADET :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(477, 94);
+            this.label3.Location = new System.Drawing.Point(417, 70);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.Size = new System.Drawing.Size(56, 15);
             this.label3.TabIndex = 9;
             this.label3.Text = "DÖKÜM :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(76, 287);
+            this.label4.Location = new System.Drawing.Point(66, 215);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(52, 20);
+            this.label4.Size = new System.Drawing.Size(41, 15);
             this.label4.TabIndex = 10;
             this.label4.Text = "ŞEKİL :";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(76, 220);
+            this.label5.Location = new System.Drawing.Point(66, 165);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.Size = new System.Drawing.Size(64, 15);
             this.label5.TabIndex = 11;
             this.label5.Text = "SERTİFİKA :";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(76, 154);
+            this.label6.Location = new System.Drawing.Point(66, 116);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(52, 20);
+            this.label6.Size = new System.Drawing.Size(44, 15);
             this.label6.TabIndex = 12;
             this.label6.Text = "ÖLÇÜ :";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(477, 220);
+            this.label7.Location = new System.Drawing.Point(417, 165);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.Size = new System.Drawing.Size(41, 15);
             this.label7.TabIndex = 13;
             this.label7.Text = "FİYAT :";
             // 
             // fiyat
             // 
-            this.fiyat.Location = new System.Drawing.Point(561, 220);
+            this.fiyat.Location = new System.Drawing.Point(491, 165);
+            this.fiyat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.fiyat.Name = "fiyat";
-            this.fiyat.Size = new System.Drawing.Size(151, 27);
+            this.fiyat.Size = new System.Drawing.Size(133, 23);
             this.fiyat.TabIndex = 14;
             // 
             // StokEkle
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 338);
             this.Controls.Add(this.fiyat);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -214,15 +194,17 @@ namespace SetApp
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_add);
             this.Controls.Add(this.adet);
             this.Controls.Add(this.dokum);
             this.Controls.Add(this.sekil);
             this.Controls.Add(this.sertifika);
             this.Controls.Add(this.olcu);
             this.Controls.Add(this.tur);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "StokEkle";
             this.Text = "Stok Ekle";
+            this.Load += new System.EventHandler(this.StokEkle_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -236,7 +218,7 @@ namespace SetApp
         private System.Windows.Forms.ComboBox sekil;
         private System.Windows.Forms.ComboBox dokum;
         private System.Windows.Forms.TextBox adet;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_add;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
--- SetApp/StokEkle.cs
+++ SetApp/StokEkle.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,11 +18,97 @@ namespace SetApp
         {
             InitializeComponent();
         }
-        public void addData()
+        private void StokEkle_Load(object sender, EventArgs e)
         {
-            String sorgu = "INSERT INTO vanalar";
+            getDatas();
+        }
+        public void addStok()
+        {
+            if (tur.SelectedIndex != -1 && olcu.SelectedIndex != -1 && sertifika.SelectedIndex != -1 && sekil.SelectedIndex != -1 && dokum.SelectedIndex != -1 && adet.Text != "" && fiyat.Text != "")
+            {
+                string addVanalar = "INSERT INTO Vanalar (turAdi, olcuAdi, sertifikaAdi, sekilAdi, dokumAdi, adet) VALUES (@p1, @p2, @p3, @p4, @p5, @p6)";
+                SqlCommand command = new SqlCommand(addVanalar, con.baglan());
+                command.Parameters.AddWithValue("@p1", tur.Text);
+                command.Parameters.AddWithValue("@p2", olcu.Text);
+                command.Parameters.AddWithValue("@p3", sertifika.Text);
+                command.Parameters.AddWithValue("@p4", sekil.Text);
+                command.Parameters.AddWithValue("@p5", dokum.Text);
+                command.Parameters.AddWithValue("@p6", adet.Text);
+                command.ExecuteNonQuery();
+                con.baglan().Close();
+                controlsClear();
+                MessageBox.Show("Kaydedildi.");
+            }
+            else
+            {
+                MessageBox.Show("Hicbir alan bos birakilamaz.");
+            }
+        }
+        public void getDatas()
+        {
+            string getTur = "SELECT * FROM VanaTurleri";
+            string getOLcu = "SELECT * FROM VanaOlculeri";
+            string getSertfika = "SELECT * FROM VanaSertifikalari";
+            string getSekil = "SELECT * FROM VanaSekilleri";
+            string getDokum = "SELECT * FROM VanaDokumTurleri";
+
+            SqlDataReader readTur;
+            SqlCommand turLoadCommand = new SqlCommand(getTur, con.baglan());
+            readTur = turLoadCommand.ExecuteReader();
+
+            SqlDataReader readOlcu;
+            SqlCommand olcuLoadCommand = new SqlCommand(getOLcu, con.baglan());
+            readOlcu = olcuLoadCommand.ExecuteReader();
+
+            SqlDataReader readSertifika;
+            SqlCommand sertifikaLoadCommand = new SqlCommand(getSertfika, con.baglan());
+            readSertifika = sertifikaLoadCommand.ExecuteReader();
+
+            SqlDataReader readSekil;
+            SqlCommand sekilLoadCommand = new SqlCommand(getSekil, con.baglan());
+            readSekil = sekilLoadCommand.ExecuteReader();
+
+            SqlDataReader readDokum;
+            SqlCommand dokumLoadCommand = new SqlCommand(getDokum, con.baglan());
+            readDokum = dokumLoadCommand.ExecuteReader();
+
+            while (readTur.Read())
+            {
+                tur.Items.Add(readTur[1].ToString());
+            }
+            while (readOlcu.Read())
+            {
+                olcu.Items.Add(readOlcu[1].ToString());
+            }
+            while (readSertifika.Read())
+            {
+                sertifika.Items.Add(readSertifika[1].ToString());
+            }
+            while (readSekil.Read())
+            {
+                sekil.Items.Add(readSekil[1].ToString());
+            }
+            while (readDokum.Read())
+            {
+                dokum.Items.Add(readDokum[1].ToString());
+            }
+
+            con.baglan().Close();
+        }
+        public void controlsClear()
+        {
+            tur.SelectedIndex = -1;
+            olcu.SelectedIndex = -1;
+            sertifika.SelectedIndex = -1;
+            sekil.SelectedIndex = -1;
+            dokum.SelectedIndex = -1;
+            adet.Clear();
+            fiyat.Clear();
         }
 
-        
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            addStok();
+        }
     }
 }
